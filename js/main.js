82934b546951ce820efca26241a4ceec5bbf35cb
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-20 20:35:06 +0200

    [UPD] Milestone 0 creato array di oggetti con tutte le informazioni

--- js/main.js
+++ js/main.js
@@ -2,3 +2,38 @@
 "use strict";
 
 console.log("Lo script funziona correttamente")
+
+//ARRAY DI OGGETTI
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founde & Ceo',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Carol',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Grapihc Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    },
+]
+
